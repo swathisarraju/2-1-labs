3b42e8193c3d21f8552b0fffd04d57cf9569703e
swathisarraju <97828446+swathisarraju@users.noreply.github.com>
2022-01-16 16:52:30 +0530

    this pointer

---
+++ C++/Experiment-7/this_op.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  
+using namespace std;  
+class Employee {  
+   public:  
+       int id; 
+       string name;  
+       float salary;  
+       Employee(int id, string name, float salary){    
+             this->id = id;    
+             this->name = name;    
+             this->salary = salary;   
+        }    
+       void display(){    
+            cout<<id<<"  "<<name<<"  "<<salary<<endl;    
+        }    
+};  
+int main(){  
+    Employee e1 =Employee(101, "Akash", 890000);      
+    e1.display();    
+    return 0;  
+}  
+
