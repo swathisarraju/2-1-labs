a9d5b2c58801c422c3f62eb2d01c53cd35476e0f
swathisarraju <97828446+swathisarraju@users.noreply.github.com>
2022-02-04 16:15:58 +0530

    Console I/O

---
+++ C++/Experiment-14/Experiment-14a/Experiment-14a.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+	char a; // used to scan a single char 
+  cout<<"Enter the character:";
+	cin.get(a); // used to put a single char onto the screen. 
+  cout << "The given Character is:";	
+  cout.put(a); // ncount character from array 
+	cout.write("\nWelcome to Hyderabad", 5); 
+
+} 
